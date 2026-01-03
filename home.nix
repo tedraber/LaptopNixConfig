26c393f371d1ab2791392c24dfcaadaaa798e708
Ted Raber <tedraber09@gmail.com>
2026-01-03 15:57:07 -0500

    Refactor package list in home.nix
    Removed duplicate entries and cleaned up package list.

--- home.nix
+++ home.nix
@@ -4,22 +4,18 @@
   home.username = "ted";
   home.homeDirectory = "/home/ted";
   
-  # This should match your NixOS version (check with: nixos-version)
   home.stateVersion = "25.11";
 
-  # Let Home Manager manage itself
   programs.home-manager.enable = true;
   
 
-  # Install packages for your user
   home.packages = with pkgs; [
-	vencord
-	vscode
-	ghostty
-	neovim-gtk
-	ripgrep
-	fd
-	lazygit
+  vencord
+  ghostty
+  neovim-gtk
+  ripgrep
+  fd
+  lazygit
   protonplus
   adwsteamgtk
   heroic
@@ -30,7 +26,6 @@
   zed-editor
   jetbrains.idea-community-bin
   ant
-  raylib-games
   kdePackages.kdeconnect-kde
   kdePackages.qt6ct
   nordic
